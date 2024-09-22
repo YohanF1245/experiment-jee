3cc8841b70df92697b4233d4a39f44ef40b4f9da
Yohan Ferdin <yohan.ferdin@hotmail.fr>
2024-09-22 14:16:10 +0200

    feat: add dynamic time and date to bonjour page

--- src/main/java/com/yferdin/servlets/Test.java
+++ src/main/java/com/yferdin/servlets/Test.java
@@ -7,6 +7,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @WebServlet("/Test")
 public class Test extends HttpServlet {
@@ -18,6 +20,13 @@ public class Test extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter formatterDate = DateTimeFormatter.ofPattern("dd-MM-YYYY");
+		DateTimeFormatter formatterTime = DateTimeFormatter.ofPattern("HH:mm:ss");
+		String dateFormated = now.format(formatterDate);
+		String timeFormated = now.format(formatterTime);
+		request.setAttribute("date", dateFormated);
+		request.setAttribute("time", timeFormated);
 		this.getServletContext().getRequestDispatcher("/WEB-INF/FirstPage.jsp").forward(request, response);
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
--- src/main/webapp/WEB-INF/FirstPage.jsp
+++ src/main/webapp/WEB-INF/FirstPage.jsp
@@ -9,5 +9,17 @@
 <body>
 	<h1>Premiere page</h1>
 	<p>Bonjour, bienvenue sur mon premier JSP</p>
+	<p>Nous sommes le 
+	<%
+		String date = (String) request.getAttribute("date");
+		out.print(date);
+	%>
+	</p>
+	<p>Il est :
+		<%
+		String time = (String) request.getAttribute("time");
+		out.print(time);
+	%>
+	</p>
 </body>
 </html>
